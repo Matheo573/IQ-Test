9f23bd478bd90f5e6d91145be5a3923288fd26c9
it1shka <2tishbel@gmail.com>
2022-12-07 15:46:07 +0100

    Initial commit

--- Tiles.java
+++
@@ -1,6 +0,0 @@
-public class Tiles {
-    
-    public Tiles(){
-
-    }
-}
---
+++ src/Application.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Application {
+    private static JFrame window;
+
+    public static void main(String[] args) {
+        window = new JFrame("IQ Test");
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        addTestPanel();
+        window.setVisible(true);
+    }
+
+    private static void addTestPanel() {
+        var panel = new JFrame()
+    }
+}
